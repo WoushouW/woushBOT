103b19f46ffd8e4143bf8b087674832ee4909f6f
WoushouW <144804974+WoushouW@users.noreply.github.com>
2025-12-11 00:39:30 +0200

    Update script.js
    123

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "/api";
 let currentPin = "";
 let currentGuildId = null;
 let staticData = null; 
@@ -594,3 +594,4 @@ function showToast(msg) {
 
 checkSession();
 renderPinDots();
+
